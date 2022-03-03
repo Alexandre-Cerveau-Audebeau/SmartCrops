359a82369860dee9ad9e921cba508823ec2e5456
Alexandre <alexaudebeau@gmail.com>
2022-03-04 00:30:33 +0100

    add plant button modal fonctionnel

--- src/SmartCrops.WebClient/client-app/src/Components/PlantModalAdd.tsx
+++ src/SmartCrops.WebClient/client-app/src/Components/PlantModalAdd.tsx
@@ -4,6 +4,8 @@ import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import axios from 'axios';
+import PlantType from '../Models/PlantType';
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -19,8 +21,17 @@ const style = {
 
 export default function AddPlantModal() {
   const [open, setOpen] = React.useState(false);
+  const [name, setName] = React.useState<string>('');
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  
+
+  async function createPlant() {
+    let response = await axios.post<PlantType>('https://localhost:7137/api/plantTypes', {
+      plantName: name 
+    });
+}
+
 
   
 
@@ -44,11 +55,18 @@ export default function AddPlantModal() {
                 <Typography id="keep-mounted-modal-description" sx={{ mt: 2 }}>
                     Ajouter une plante
                 </Typography>
+                
                 <TextField
                   required
                   id="outlined-required"
                   label="Plant Name Required"
+                  value={name}
+                  onChange={(event)=>setName(event.target.value)}
                 />
+                <Button
+                onClick={createPlant}>
+                  Ajouter
+                </Button>
          </Box>
       </Modal>
     </div>
